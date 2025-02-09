f578c3d2f326e728cdd34741265eddeb2ff2cc6d
sehxxnee <shyan5253@naver.com>
2025-02-10 03:07:12 +0900

    chore: setup Vite + TypeScript + React + Tailwind + DaisyUI + Prettier + ESLint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: globals.browser }},
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
